8da81f74db1337dd6279ce6f18ca8b4f2e4a403a
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-08-24 20:36:09 +0200

    chore: use konva for canvas rendering

--- src/views/System.tsx
+++ src/views/System.tsx
@@ -1,22 +1,20 @@
 import * as React from 'react';
-import { useRef, useEffect } from 'react';
-import { Game } from '../models/Game';
+import {
+    Stage,
+    Layer, Text,
+} from 'react-konva';
+import { useWindowSize } from '../hooks';
 
 export const System = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        if (canvas == null) {
-            throw Error("Expected canvas to exist");
-        }
-        new Game(canvas);
-    })
-
-    return <div>
-        <canvas
-            width={window.innerWidth}
-            height={window.innerHeight}
-            ref={canvasRef}
-        />
-    </div>
-}
+    const {
+        height, width,
+    } = useWindowSize();
+    return (
+        <Stage width={width} height={height}>
+            <Layer>
+                <Text text="Try click on rect" />
+                {/* <ColoredRect /> */}
+            </Layer>
+        </Stage>
+    );
+};
