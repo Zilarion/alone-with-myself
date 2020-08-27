4574a96a291b4609d23e0f6138d792d534078a8e
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-08-27 21:06:02 +0200

    chore: introduce distanceBetween util

--- src/util/distanceBetween.ts
+++ src/util/distanceBetween.ts
@@ -1,3 +1,3 @@
 import { Vector } from '../models';
 
-export const distanceBetween = (a: Vector, b: Vector) => Math.sqrt(a.x ** b.x + a.y ** b.y);
+export const distanceBetween = (a: Vector, b: Vector) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
