556b88599544e2bebe6e00624d38f5a54e406ceb
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-09-20 21:06:15 +0200

    feat: add activation state to resource points

--- src/components/ResourcePointSummary.tsx
+++ src/components/ResourcePointSummary.tsx
@@ -2,6 +2,7 @@ import { observer } from 'mobx-react-lite';
 import * as React from 'react';
 
 import { ResourcePoint } from '../models';
+import { Button } from './Button';
 import { Header } from './Header';
 import { LabelValue } from './LabelValue';
 
@@ -9,7 +10,35 @@ interface ResourcePointSummaryProps {
     point: ResourcePoint;
 }
 
-export const ResourcePointSummary = observer(({ point: { resources } }: ResourcePointSummaryProps) => {
+export const ResourcePointSummary = observer(({
+    point: {
+        resources,
+        miners,
+        printers,
+        operational,
+        activate,
+    },
+}: ResourcePointSummaryProps) => {
+    const content = operational ? (
+        <>
+            <LabelValue
+                label="Miners"
+                value={`${ miners } miner(s) operational`}
+            />
+            <Button>Print miner</Button>
+
+            <LabelValue
+                label="Printers"
+                value={`${ printers } printer(s) operational`}
+            />
+
+            <Button>Print printer</Button>
+        </>
+    ) : (
+        <>
+            <Button onClick={activate}>Initiate mining procedures</Button>
+        </>
+    );
     return (
         <div>
             <Header>Resource point</Header>
@@ -17,6 +46,7 @@ export const ResourcePointSummary = observer(({ point: { resources } }: Resource
                 label="Resources"
                 value={`${ resources.toFixed(0) } kg`}
             />
+            { content }
         </div>
     );
 });
--- src/models/ResourcePoint.ts
+++ src/models/ResourcePoint.ts
@@ -1,4 +1,10 @@
 import {
+    action,
+    computed,
+    observable,
+} from 'mobx';
+
+import {
     InteractionPoint,
     InteractionPointProps,
 } from './InteractionPoint';
@@ -8,14 +14,47 @@ type ResourcePointProps = {
 } & InteractionPointProps;
 
 export class ResourcePoint extends InteractionPoint {
+    @observable
     private _resources: number;
+
+    @observable
+    private _printers: number = 0;
+
+    @observable
+    private _miners: number = 0;
+
+    @observable
+    private _operational: boolean = false;
 
     constructor(props: ResourcePointProps) {
         super(props);
         this._resources = props.resources;
     }
 
+    @computed
+    public get printers() {
+        return this._printers;
+    }
+
+    @computed
+    public get miners() {
+        return this._miners;
+    }
+
+    @computed
     public get resources() {
         return this._resources;
+    }
+
+    @computed
+    public get operational() {
+        return this._operational;
+    }
+
+    @action.bound
+    public activate() {
+        this._operational = true;
+        this._miners = 1;
+        this._printers = 1;
     }
 }
