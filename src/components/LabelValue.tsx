fec9ef252873a2b365c4c90badae52f45241ed1e
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-09-24 19:53:30 +0200

    fix: remove label value margin

--- src/components/LabelValue.tsx
+++ src/components/LabelValue.tsx
@@ -6,11 +6,6 @@ interface LabelValueProps {
     label: string;
     value: string | JSX.Element;
 }
-
-const StyledWrapper = styled.div`
-    margin: ${p => p.theme.margin.small}
-`;
-
 const StyledLabel = styled.div`
     color: ${p => p.theme.color.header};
     font-size: 16px;
@@ -24,8 +19,8 @@ const StyledValue = styled.div`
 export function LabelValue({
     value, label,
 }: LabelValueProps) {
-    return (<StyledWrapper>
+    return (<div>
         <StyledLabel>{label}</StyledLabel>
         <StyledValue>{value}</StyledValue>
-    </StyledWrapper>);
+    </div>);
 }
