67136f8c7c9d4fb1d80eb981ff1c3157e36bba22
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-09-24 19:36:14 +0200

    feat: add FormattedResource component

---
+++ src/components/FormattedResource.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { FormattedNumber } from 'react-intl';
+
+import {
+    RESOURCE_TO_UNIT,
+    ResourceType,
+} from '../models';
+import { assert } from '../util';
+
+interface FormattedResourceProps {
+    value: number;
+    type: ResourceType;
+}
+
+export function FormattedResource({
+    value,
+    type,
+}: FormattedResourceProps) {
+    const unit = RESOURCE_TO_UNIT.get(type);
+    assert(unit != null, `Expected ${type} to have a unit.`);
+    return <FormattedNumber
+        value={value}
+        style="unit"
+        unit={unit}
+    />;
+}
