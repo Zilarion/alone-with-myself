7c2111c4836cf7c214bd7dc14df1e03783b78013
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-09-24 20:15:45 +0200

    feat: button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,32 @@
 import * as React from 'react';
 
+import styled from '../themed-components';
+
+const StyledButton = styled.button`
+    border: 1px solid ${p => p.theme.color.primary};
+    color: ${p => p.theme.color.primary};
+    background: none;
+    transition: all 0.8s;
+    outline: none;
+
+    &:hover {
+        border-color: 1px solid ${p => p.theme.color.primaryLight};
+        color: ${p => p.theme.color.primaryLight};
+        cursor: pointer;
+    }
+
+    &:active {
+        box-shadow: 0 0 5px ${p => p.theme.color.primaryLight};
+        transition-duration: 0.05s;
+    }
+    
+    &:disabled {
+        border: 1px solid ${p => p.theme.color.disabled};
+        color: ${p => p.theme.color.disabled};
+        cursor: auto;
+    }
+`;
+
 type ButtonProps = React.PropsWithChildren<{
     onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
     disabled?: boolean;
@@ -10,10 +37,10 @@ export function Button({
     disabled,
     children,
 }: ButtonProps) {
-    return <button
+    return <StyledButton
         onClick={onClick}
         disabled={disabled}
     >
         {children}
-    </button>;
+    </StyledButton>;
 }
