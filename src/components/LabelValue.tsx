2fb524b3cb19e79d5ec080311e012bb17229a417
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-09-24 19:55:14 +0200

    fix: only vertical margin should be used

--- src/components/LabelValue.tsx
+++ src/components/LabelValue.tsx
@@ -6,6 +6,11 @@ interface LabelValueProps {
     label: string;
     value: string | JSX.Element;
 }
+
+const StyledWrapper = styled.div`
+    margin: ${p => p.theme.margin.small} 0;
+`;
+
 const StyledLabel = styled.div`
     color: ${p => p.theme.color.header};
     font-size: 16px;
@@ -19,8 +24,8 @@ const StyledValue = styled.div`
 export function LabelValue({
     value, label,
 }: LabelValueProps) {
-    return (<div>
+    return (<StyledWrapper>
         <StyledLabel>{label}</StyledLabel>
         <StyledValue>{value}</StyledValue>
-    </div>);
+    </StyledWrapper>);
 }
