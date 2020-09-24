63f0c06d893b510dac0c2e23cbc2450c94fc0170
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-09-24 19:33:52 +0200

    chore: add assert utility

---
+++ src/util/assert.ts
@@ -0,0 +1,14 @@
+
+class AssertError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = 'AssertError';
+    }
+}
+
+export function assert(condition: boolean, message: string): asserts condition {
+    if (condition) {
+        return;
+    }
+    throw new AssertError(`Assert: ${message}`);
+}
--- src/util/index.ts
+++ src/util/index.ts
@@ -2,3 +2,4 @@ export * from './createSatelliteOf';
 export * from './random';
 export * from './distanceBetween';
 export * from './emptyArray';
+export * from './assert';
