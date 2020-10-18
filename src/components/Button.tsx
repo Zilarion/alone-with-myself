1d4187755216f718aef81a753fc171f2941f5fe7
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-10-18 20:27:18 +0200

    feat: use material ui buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,37 +1,8 @@
 
+import MuiButton from '@material-ui/core/Button';
 import Tippy from '@tippyjs/react';
 
 import * as React from 'react';
-
-import styled from '../themed-components';
-
-const StyledButton = styled.button`
-    border: 1px solid ${p => p.theme.color.primary};
-    color: ${p => p.theme.color.primary};
-    background: none;
-    transition: all 0.8s;
-    outline: none;
-
-    &:hover {
-        border-color: 1px solid ${p => p.theme.color.primaryLight};
-        color: ${p => p.theme.color.primaryLight};
-        cursor: pointer;
-    }
-
-    &:active {
-        box-shadow: 0 0 5px ${p => p.theme.color.primaryLight};
-        transition-duration: 0.05s;
-    }
-`;
-
-const DisabledButton = styled.button`
-    border: 1px solid ${p => p.theme.color.disabled};
-    color: ${p => p.theme.color.disabled};
-    cursor: auto;
-    background: none;
-    transition: all 0.8s;
-    outline: none;
-`;
 
 type ButtonProps = React.PropsWithChildren<{
     onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
@@ -45,11 +16,14 @@ export function Button({
     children,
     tooltip,
 }: ButtonProps) {
-    const Btn = disabled ? DisabledButton : StyledButton;
-
     return <Tippy content={tooltip ?? children}>
-        <Btn onClick={onClick}>
+        <MuiButton
+            variant="outlined"
+            onClick={onClick}
+            color="primary"
+            disabled={disabled}
+        >
             {children}
-        </Btn>
+        </MuiButton>
     </Tippy>;
 }
