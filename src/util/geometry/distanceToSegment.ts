80e99e97513755f709047f0a8583ef88e72b1875
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-10-18 20:45:46 +0200

    feat: introduce distanceToSegment utility

---
+++ src/util/geometry/distanceToSegment.ts
@@ -0,0 +1,24 @@
+// Source:
+// https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment
+import { Vector } from '../../models';
+
+function dist2(v: Vector, w: Vector) {
+    return Math.pow(v.x - w.x, 2) + Math.pow(v.y - w.y, 2);
+}
+
+function distToSegmentSquared(p: Vector, v: Vector, w: Vector) {
+    const l2 = dist2(v, w);
+    if (l2 === 0) {
+        return dist2(p, v);
+    }
+    let t = ((p.x - v.x) * (w.x - v.x) + (p.y - v.y) * (w.y - v.y)) / l2;
+    t = Math.max(0, Math.min(1, t));
+    return dist2(p, {
+        x: v.x + t * (w.x - v.x),
+        y: v.y + t * (w.y - v.y),
+    });
+}
+
+export function distanceToSegment(p: Vector, v: Vector, w: Vector) {
+    return Math.sqrt(distToSegmentSquared(p, v, w));
+}
