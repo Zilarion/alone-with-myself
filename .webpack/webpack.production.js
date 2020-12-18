c1b8febfd33c3217679a26b8d1aa378a6379dfa5
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-12-18 23:55:00 +0100

    ci: split development and production tsconfig

--- .webpack/webpack.production.js
+++ .webpack/webpack.production.js
@@ -1,9 +1,15 @@
 const TerserPlugin = require('terser-webpack-plugin');
+const TSLoader = require('./ts-loader.config');
 
 const production = {
     mode: 'production',
     devtool: 'source-map',
     optimization: { minimizer: [ new TerserPlugin() ] },
+    module: {
+        rules: [
+            TSLoader('tsconfig.prod.json'),
+        ],
+    },
     output: {
         filename: '[name].[contenthash].min.js',
         libraryTarget: 'umd',
