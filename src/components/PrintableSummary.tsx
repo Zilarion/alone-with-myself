5362b510caf77d5417e44c50316e04a75c087b0c
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-12-19 17:14:00 +0100

    fix: disable buttons if printables are not affordable

--- src/components/PrintableSummary.tsx
+++ src/components/PrintableSummary.tsx
@@ -16,10 +16,12 @@ export const PrintableSummary = observer(({
     printables,
 }: PrintableSummaryProps) => {
     const data = tasks.map((task) => {
+        const disabled = task.maxAffordable === 0;
         const printable = printables.find((printable) => task.printable === printable);
         assert(printable != null, 'Failed to find related printable of a print task.');
         return [
             <ProgressButton
+                disabled={disabled}
                 fullWidth={true}
                 key={0}
                 onClick={() => {
