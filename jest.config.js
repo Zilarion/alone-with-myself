334fae93d5464909d853968dfc219ed738030e00
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-12-20 15:50:48 +0100

    chore: switch to jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const { defaults } = require('jest-config');
+
+module.exports = {
+    ...defaults,
+    preset: 'ts-jest',
+    coverageDirectory: './coverage',
+    collectCoverageFrom: [
+        '**/*.{ts,tsx}',
+        '!**/test/**',
+        '!**/index.{ts,tsx}',
+        '!**/examples/*.{ts,tsx}',
+    ],
+    testMatch: [
+        '**/?(*.)+(spec).[jt]s?(x)',
+    ],
+    moduleNameMapper: { '^src/(.*)$': '<rootDir>/src/$1' },
+    setupFilesAfterEnv: [ 'jest-enzyme' ],
+    testEnvironment: 'enzyme',
+    testEnvironmentOptions: { enzymeAdapter: 'react17' },
+};
