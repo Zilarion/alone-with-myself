8db00d57356f32a7ae06d39748fd83be6d510a14
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-12-31 13:08:13 +0100

    feat: add multiple selector to transporter

--- src/components/TransporterActions.tsx
+++ src/components/TransporterActions.tsx
@@ -1,66 +1,15 @@
-import { IconButton } from '@material-ui/core';
-import {
-    Add,
-    Remove,
-} from '@material-ui/icons';
 
 import { observer } from 'mobx-react-lite';
+import { useState } from 'react';
 
-import {
-    ResourceSet,
-    ResourceType,
-    Transporter,
-} from '../internal';
+import { Transporter } from '../internal';
 import { Card } from './Card';
-import { FormattedResource } from './FormattedResource';
-import { Table } from './Table';
+import { MultipleSelector } from './MultipleSelector';
+import { ResourceSetSpeedSummary } from './ResourceSetSpeedSummary';
 
 interface TransporterActionsProps {
     transporter: Transporter;
 }
-
-interface ResourceSetSpeedSummaryProps {
-    speedOf: (type: ResourceType) => number;
-    resources: ResourceSet;
-    setSpeed: (type: ResourceType, amount: number) => void;
-}
-const ResourceSetSpeedSummary = observer(({
-    speedOf,
-    resources,
-    setSpeed,
-}: ResourceSetSpeedSummaryProps) => {
-    const data = resources.map(({ type }) => {
-        const amount = speedOf(type);
-        return [
-            type,
-            <>
-                <FormattedResource
-                    key={type}
-                    value={amount}
-                    type={type}
-                    compact={true}
-                />/s
-            </>,
-            <IconButton
-                key={0}
-                size="small"
-                disabled={amount === 0}
-                onClick={() => setSpeed(type, Math.max(amount - 1, 0))}
-            >
-                <Remove />
-            </IconButton>,
-            <IconButton
-                key={1}
-                size="small"
-                onClick={() => setSpeed(type, amount + 1)}
-            >
-                <Add />
-            </IconButton>,
-        ];
-    });
-
-    return <Table data={data} />;
-});
 
 export const TransporterActions = observer(({
     transporter: {
@@ -69,11 +18,17 @@ export const TransporterActions = observer(({
         from,
     },
 }: TransporterActionsProps) => {
+    const [ amount, setAmount ] = useState(1);
     return <Card header="Transport route">
+        <MultipleSelector
+            onChange={(newValue) => setAmount(newValue)}
+            value={amount}
+        />
         <ResourceSetSpeedSummary
             speedOf={speedOf}
             resources={from.storage.resources}
             setSpeed={setSpeedPerSecond}
+            changePerClick={amount}
         />
     </Card>;
 
