fb30827441ae34e4f952c556d191ccc34b84bdb6
Ruud Andriessen <ruud.andriessen@gmail.com>
2021-12-10 16:39:33 +0100

    chore: remove mui theme adapter

--- src/theme.ts
+++ src/theme.ts
@@ -1,25 +1,3 @@
-import { createTheme, adaptV4Theme } from '@mui/material';
+import { createTheme } from '@mui/material';
 
-export const theme = createTheme(adaptV4Theme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: '#4CF2F6',
-            light: '#ACF9FB',
-        },
-        success: {
-            main: '#009A00',
-            light: '#00FF02',
-        },
-        error: {
-            main: '#990000',
-            light: '#FF0000',
-        },
-        background: { default: 'rgba(3, 26, 31, 0.6)' },
-        text: {
-            primary: '#26dafd',
-            disabled: '#666666',
-        },
-    },
-    spacing: 4,
-}));
+export const theme = createTheme({ palette: { mode: 'dark' } });
