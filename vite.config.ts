41035ce61e0ec6f6ae79a92a97e35ab0d8cbec42
Ruud Andriessen <ruud.andriessen@gmail.com>
2022-11-20 11:54:38 +0100

    build: switch to vitest

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,22 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    transformMode: {
+      web: [/\.[jt]sx?$/],
+    },
+    setupFiles: './setupVitest.ts',
+    // if you have few tests, try commenting one
+    // or both out to improve performance:
+    threads: false,
+    isolate: false,
+    coverage: {
+      provider: 'c8',
+    }
+  },
+  resolve: {
+    conditions: ['development', 'browser'],
+  },
 });
